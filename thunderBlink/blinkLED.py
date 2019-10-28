f52c05328c584e4281885366d6a67416fd7f61f0
Belife2012 <790541750@qq.com>
2019-10-28 15:36:51 +0800

    增加IDE 使用的mPythonCommand.json

--- thunderBlink/blinkLED.py
+++ thunderBlink/blinkLED.py
@@ -1,6 +1,7 @@
 from machine import Pin
 import time
 
+# 主控绿色指示灯闪烁
 def ledBlink(interval=0.5):
     LED = Pin(2, Pin.OUT)
     while True:
@@ -10,7 +11,7 @@ def ledBlink(interval=0.5):
         time.sleep(interval)
 
 if __name__ == "__main__":
-    print("LED is blinking...")
+    print("LED is blinking（绿灯正在闪烁）...")
     ledBlink()
 else:
     print(__name__ + " module is included!!!")
